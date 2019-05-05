f48287b12e57c6fcd7d7d44a4331392d20fad6f5
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2019-05-05 19:26:22 -0300

    Tests working in CI for webapp (real test still missing)

---
+++ webclient/tests/home-loads.test.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Main from '../src/components/main'
+import Home from '../src/components/home'
+
+import EthAccountManager from '../src/eth_managers/eth_account_manager'
+import DAppChainAccountManager from '../src/dc_managers/dc_account_manager'
+
+import EthCardManager from '../src/eth_managers/eth_card_manager'
+import DAppChainCardManager from '../src/dc_managers/dc_card_manager'
+
+import EthGatewayManager from '../src/eth_managers/eth_gateway_manager'
+import DAppChainGatewayManager from '../src/dc_managers/dc_gateway_manager'
+
+import EthTokenManager from '../src/eth_managers/eth_token_manager'
+import DAppChainTokenManager from '../src/dc_managers/dc_token_manager'
+
+import DAppChainSimpleStakeManager from '../src/dc_managers/dc_simple_stake_manager'
+
+import EthTokens from '../src/components/eth_tokens'
+import DAppChainTokens from '../src/components/dappchain_tokens'
+import GatewayTokens from '../src/components/gateway_tokens'
+;(async () => {
+  console.log('Loading ...')
+  const t = setTimeout(
+    () =>
+      console.log(
+        '\n\n----> If this takes too long to start, please try to reset MetaMask cache :)'
+      ),
+    5000
+  )
+
+  const ethAccountManager = await EthAccountManager.createAsync()
+  const dcAccountManager = await DAppChainAccountManager.createAsync()
+
+  const ethCardManager = await EthCardManager.createAsync()
+  const dcCardManager = await DAppChainCardManager.createAsync()
+
+  const ethTokenManager = await EthTokenManager.createAsync()
+  const dcTokenManager = await DAppChainTokenManager.createAsync()
+
+  const ethGatewayManager = await EthGatewayManager.createAsync()
+  const dcGatewayManager = await DAppChainGatewayManager.createAsync()
+
+  const dcSimpleStakeManager = await DAppChainSimpleStakeManager.createAsync()
+
+  clearTimeout(t)
+
+  const BuildMain = () => (
+    <Main
+      ethAccountManager={ethAccountManager}
+      ethCardManager={ethCardManager}
+      dcAccountManager={dcAccountManager}
+      dcCardManager={dcCardManager}
+    />
+  )
+
+  const BuildHome = () => (
+    <Home ethAccountManager={ethAccountManager} dcAccountManager={dcAccountManager} />
+  )
+
+  const BuildEthTokens = () => (
+    <EthTokens
+      ethGatewayManager={ethGatewayManager}
+      ethAccountManager={ethAccountManager}
+      dcAccountManager={dcAccountManager}
+      ethCardManager={ethCardManager}
+      dcCardManager={dcCardManager}
+      ethTokenManager={ethTokenManager}
+      dcTokenManager={dcTokenManager}
+    />
+  )
+
+  const BuildGatewayTokens = () => (
+    <GatewayTokens
+      ethAccountManager={ethAccountManager}
+      dcAccountManager={dcAccountManager}
+      ethCardManager={ethCardManager}
+      dcCardManager={dcCardManager}
+      ethTokenManager={ethTokenManager}
+      dcTokenManager={dcTokenManager}
+      ethGatewayManager={ethGatewayManager}
+      dcGatewayManager={dcGatewayManager}
+    />
+  )
+
+  const BuildDAppChainTokens = () => (
+    <DAppChainTokens
+      ethAccountManager={ethAccountManager}
+      dcAccountManager={dcAccountManager}
+      ethCardManager={ethCardManager}
+      dcCardManager={dcCardManager}
+      ethTokenManager={ethTokenManager}
+      dcTokenManager={dcTokenManager}
+      dcGatewayManager={dcGatewayManager}
+      dcSimpleStakeManager={dcSimpleStakeManager}
+    />
+  )
+
+  ReactDOM.render(
+    <Router>
+      <div>
+        <header>
+          <BuildMain />
+        </header>
+
+        <main role="main" style={{ marginTop: 100 }}>
+          <div className="container">
+            <Route exact path="/" component={BuildHome} />
+            <Route path="/eth" component={BuildEthTokens} />
+            <Route path="/gateway" component={BuildGatewayTokens} />
+            <Route path="/dappchain" component={BuildDAppChainTokens} />
+          </div>
+        </main>
+      </div>
+    </Router>,
+    document.getElementById('root')
+  )
+})()
+
+function sum(a, b) {
+  return a + b;
+}
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});

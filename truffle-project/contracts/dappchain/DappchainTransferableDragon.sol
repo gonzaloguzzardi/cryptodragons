047cd28438c7e25d9ad127c96339edb552a7f3a6
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2019-09-29 15:56:02 -0300

    add mapping functions

--- truffle-project/contracts/dappchain/DappchainTransferableDragon.sol
+++ truffle-project/contracts/dappchain/DappchainTransferableDragon.sol
@@ -9,8 +9,10 @@ contract IDappchainGateway {
 
 contract DappchainTransferableDragon is DragonFactory {
     address private _gateway;
+    mapping (address => address) private _mainnetMapping;
 
     constructor(address gateway) DragonBase() public {
+        require(gateway != address(0), "Invalid gateway address");
         _gateway = gateway;
     }
 
@@ -18,6 +20,18 @@ contract DappchainTransferableDragon is DragonFactory {
     function setGatewayAddress(address gateway) external onlyOwner {
         //TODO check address is a gateway
         _gateway = gateway;
+    }
+
+    function undoMapping(address owner, address mainnetAddress) external onlyOwner {
+        require(owner != address(0), "Invalid owner address to be mapped");
+        require(mainnetAddress != address(0), "Invalid mainnet address to be mapped");
+        _mainnetMapping[owner] = mainnetAddress;
+    }
+
+    function mapContractToMainnet(address mainnetAddress) external {
+        require(mainnetAddress != address(0), "Invalid mainnet address to be mapped");
+        require(_mainnetMapping[msg.sender] != address(0), "Address already mapped. Request owner to undo the mapping");
+        _mainnetMapping[msg.sender] = mainnetAddress;
     }
 
     // Used by the DAppChain Gateway to mint tokens that have been deposited to the Ethereum Gateway
--- truffle-project/contracts/mainnet/MainnetTransferableDragon.sol
+++ truffle-project/contracts/mainnet/MainnetTransferableDragon.sol
@@ -9,14 +9,28 @@ contract IMainnetGateway {
 
 contract MainnetTransferableDragon is DragonFactory {
     address private _gateway;
+    mapping (address => address) private _sidechainMapping;
 
     constructor(address gateway) DragonBase() public {
+        require(gateway != address(0), "Invalid gateway address");
         _gateway = gateway;
     }
 
     // Setter to update who the gateway is
     function setGatewayAddress(address gateway) external onlyOwner {
         _gateway = gateway;
+    }
+
+    function undoMapping(address owner, address sidechainAddress) external onlyOwner {
+        require(owner != address(0), "Invalid owner address to be mapped");
+        require(sidechainAddress != address(0), "Invalid sidechain address to be mapped");
+        _sidechainMapping[owner] = sidechainAddress;
+    }
+
+    function mapContractToSidechain(address sidechainAddress) external {
+        require(sidechainAddress != address(0), "Invalid sidechain address to be mapped");
+        require(_sidechainMapping[msg.sender] != address(0), "Address already mapped. Request owner to undo the mapping");
+        _sidechainMapping[msg.sender] = sidechainAddress;
     }
 
     function transferToGateway(uint256 _tokenId) public {

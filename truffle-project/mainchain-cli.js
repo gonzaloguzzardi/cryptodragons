dbd62cbd1687f5360bfa5ce408a0e9d2aa9fb6d1
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2020-01-04 16:47:34 -0300

    Mainchain API (create + list)

--- truffle-project/mainchain-cli.js
+++ truffle-project/mainchain-cli.js
@@ -2,6 +2,7 @@ const Web3 = require('web3')
 const program = require('commander')
 const fs = require('fs')
 const path = require('path')
+const WAsync = require('@rimiti/express-async');
 
 const MainchainDragonTokenJson = require('./src/contracts/MainnetTransferableDragon.json')
 
@@ -80,3 +81,63 @@ program
 program
   .version('0.1.0')
   .parse(process.argv)
+
+
+// API SERVER
+var express = require('express')
+var http = require('http')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.status(200).send("Welcome to API REST")
+})
+
+app.get('/api/dragon/create',  WAsync.wrapAsync(async function createFunction(req, res, next) {
+  const { account, web3js } = loadGanacheAccount();
+  let hash = "";
+  try {
+    const tx = await createDragonToken(web3js, account, req.query.gas || 350000);
+    console.log(`Dragon created`);
+    console.log(`tx hash: ${tx.transactionHash}`);
+    hash = tx.transactionHash;
+    res.status(200).send(hash)
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}))
+
+//app.get('/api/dragon/transfer', WAsync.wrapAsync(async function transferFunction(req, res, next) {
+  // const { account, web3js, client } = loadLoomAccount(req.query.account)
+  // try {
+  //   if (req.query.id === undefined) {
+  //   }
+  //   const data = await transferDragonToGateway(web3js, req.query.gas || 350000, account, req.query.id)
+
+  //   console.log(`\n Token with id ${req.query.id} was successfully transfered to gateway \n`) 
+  // } catch (err) {
+  //   res.status(400).send(err)
+  // } finally {
+  //   if (client) {
+  //     client.disconnect()
+  //   }
+  //   res.status(200).send(`Token with id ${req.query.id} was successfully transfered to gateway`)
+  // }
+//}))
+
+app.get('/api/dragons', WAsync.wrapAsync(async function getDragonFunction(req, res, next) {
+  const { account, web3js } = loadGanacheAccount();
+  try {
+    const data = await getMyDragons(web3js, account, req.query.gas || 350000);
+    console.log(`\nAddress ${account} holds dragons with id ${data}\n`);
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}))
+
+http.createServer(app).listen(8001, () => {
+  console.log('Server started at http://localhost:8001');
+});

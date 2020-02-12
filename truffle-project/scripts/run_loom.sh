ff87e9fae139d568ce15b43ed1ff80fe62204083
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2020-02-11 21:43:46 -0300

    add helper methods

--- truffle-project/scripts/run_loom.sh
+++ truffle-project/scripts/run_loom.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# Pull docker image if it doesn't exist
+if [[ "$(docker images -q gonzaloguzzardi/cryptodragons:loom_node_v001 2> /dev/null)" == "" ]]; then
+    docker pull gonzaloguzzardi/cryptodragons:loom_node_v001
+fi
+
+docker run -d -p 46656:46656  -p 46658:46658  gonzaloguzzardi/cryptodragons:loom_node_v001

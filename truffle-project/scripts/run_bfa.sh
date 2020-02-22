9bd822acda066cd7a6d8bec39a7fcbdf001be98d
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2020-02-22 20:03:27 -0300

    Remove ganache + webpack + dist web from truffle-project

--- truffle-project/scripts/run_bfa.sh
+++ truffle-project/scripts/run_bfa.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 # Remember to run  docker pull gonzaloguzzardi/cryptodragons:bfa_node_v002 before
-docker run -p 8545:8545 -p 8546:8546 -p 8547:8547 -p 30303:30303 -i -t gonzaloguzzardi/cryptodragons:bfa_node_v002
+docker run -d -p 8545:8545 -p 8546:8546 -p 8547:8547 -p 30303:30303 -i -t gonzaloguzzardi/cryptodragons:bfa_node_v002

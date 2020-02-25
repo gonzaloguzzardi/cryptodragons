5d2cc25a6550318acea40d407e76135dcb433d06
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2020-02-25 17:24:21 -0300

    Fix logic to return events in gateways

--- oracle/controllers/index.js
+++ oracle/controllers/index.js
@@ -3,26 +3,22 @@ const {
 } = require('../config');
 const { collectEventsFromSidechainGateway, collectEventsFromMainchainGateway } = require('../mongo-utils');
 
-function getDragonsFromSidechainGateway() {
-	return new Promise((res, rej) => {
-		collectEventsFromSidechainGateway(database, mongoUrl, collection)
-			.then(result => res(result))
-			.catch(err => rej(err));
-	});
-}
+const getDragonsFromSidechainGateway = new Promise((res, rej) => {
+	collectEventsFromSidechainGateway(database, mongoUrl, collection)
+		.then(result => res({ 'sidechain-gateway-results': result }))
+		.catch(err => rej(err));
+});
 
-function getDragonsFromMainchainGateway() {
-	return new Promise((res, rej) => {
-		collectEventsFromMainchainGateway(database, mongoUrl, collection)
-			.then(result => res(result))
-			.catch(err => rej(err));
-	});
-}
+const getDragonsFromMainchainGateway = new Promise((res, rej) => {
+	collectEventsFromMainchainGateway(database, mongoUrl, collection)
+		.then(result => res({ 'mainchain-gateway-results': result }))
+		.catch(err => rej(err));
+});
 
 function getDragonsInGateways(req, res) {
 	Promise.all([ getDragonsFromSidechainGateway, getDragonsFromMainchainGateway ])
-		.then(values => res.send(values))
-		.catch(err => console.error(err));
+		.then(values => res.status(200).send(values))
+		.catch(err => res.status(500).send(err));
 }
 
 module.exports = {

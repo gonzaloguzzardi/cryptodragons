95d6928ee62a7d8b1e58b145659d46824473e1cb
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2020-03-28 18:58:05 -0300

    Update bfa docker versionÇ2

--- truffle-project/scripts/run_bfa.sh
+++ truffle-project/scripts/run_bfa.sh
@@ -1,4 +1,10 @@
 
 #!/bin/bash
-# Remember to run  docker pull gonzaloguzzardi/cryptodragons:bfa_node_v003 before
+# Remember to run  docker pull gonzaloguzzardi/cryptodragons:bfa_node_v004 before
+
+# Pull docker image if it doesn't exist
+if [[ "$(docker images -q gonzaloguzzardi/cryptodragons:bfa_node_v004 2> /dev/null)" == "" ]]; then
+    docker pull gonzaloguzzardi/cryptodragons:bfa_node_v004
+fi
+
 docker run -d -p 8545:8545 -p 8546:8546 -p 8547:8547 -p 30303:30303 -i -t gonzaloguzzardi/cryptodragons:bfa_node_v004

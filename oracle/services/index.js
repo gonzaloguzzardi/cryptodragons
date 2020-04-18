18b8cd5c1bd985abbea2f8d00c39d74cab4a18c8
fernandonitz <nitzfernando@gmail.com>
2020-04-18 15:30:46 -0300

    taking away dependencies from oracle

--- oracle/services/index.js
+++ oracle/services/index.js
@@ -1,8 +1,6 @@
 const collectFromSidechainGatewayAndSendToMainchain = require('./collect-from-sidechain-gateway-and-send-to-mainchain');
 const collectFromMainchainGatewayAndSendToSidechain = require('./collect-from-mainchain-gateway-and-send-to-sidechain');
 
-const listenSideChainEvents = require('./listen-sidechain-events');
-const listenMainChainEvents = require('./listen-mainchain-events');
 const {
     insertDragon,
     deleteADragon
@@ -13,6 +11,4 @@ module.exports = {
     collectFromMainchainGatewayAndSendToSidechain,
 	deleteADragon,
 	insertDragon,
-    listenSideChainEvents,
-    listenMainChainEvents,
 };

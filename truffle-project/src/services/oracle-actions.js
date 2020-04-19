47837a9242bab33c27ce7c782ece4ccba1d15c80
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2020-04-19 13:10:42 -0300

    tabs formatting

--- truffle-project/src/services/oracle-actions.js
+++ truffle-project/src/services/oracle-actions.js
@@ -1,23 +1,20 @@
 const axios = require('axios');
-const {
-    oracleApiPort,
-    oracleApiUrl,
-} = require('../config');
+const { oracleApiPort, oracleApiUrl } = require('../config');
 
 function saveDragonOnOracle(dragon) {
-    axios.post(`${oracleApiUrl}:${oracleApiPort}/api/saveDragon`, dragon);
-} 
+	axios.post(`${oracleApiUrl}:${oracleApiPort}/api/saveDragon`, dragon);
+}
 
 function deleteDragonInOracle(dragon) {
-    axios.post(`${oracleApiUrl}:${oracleApiPort}/api/deleteDragon`, dragon);
-} 
+	axios.post(`${oracleApiUrl}:${oracleApiPort}/api/deleteDragon`, dragon);
+}
 
 function insertDragonInOracle(dragon) {
-    axios.post(`${oracleApiUrl}:${oracleApiPort}/api/insertDragon`, dragon);
-} 
+	axios.post(`${oracleApiUrl}:${oracleApiPort}/api/insertDragon`, dragon);
+}
 
 module.exports = {
-    saveDragonOnOracle,
-    deleteDragonInOracle,
-    insertDragonInOracle
+	saveDragonOnOracle,
+	deleteDragonInOracle,
+	insertDragonInOracle,
 };

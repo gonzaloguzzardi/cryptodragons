2feb82bf7cbf47ea1bbe134e52309a0d2f6ae8af
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2020-04-19 14:06:33 -0300

    truffle-project: Remove dep '@rimiti/express-async'

--- truffle-project/mainchain-cli.js
+++ truffle-project/mainchain-cli.js
@@ -1,7 +1,6 @@
 const Web3 = require('web3');
 const fs = require('fs');
 const path = require('path');
-const WAsync = require('@rimiti/express-async');
 
 // API SERVER
 const express = require('express');
@@ -42,127 +41,103 @@ function loadGanacheAccount() {
 	return { account: ownerAccount, web3js };
 }
 
-app.get(
-	'/api/dragon/create',
-	WAsync.wrapAsync(async function createFunction(req, res, next) {
-		const { account, web3js } = loadGanacheAccount();
-		let hash = '';
-		try {
-			const tx = await createDragonToken(web3js, account, req.query.gas || 350000);
-			console.log(`Dragon created`);
-			console.log(`tx hash: ${tx.transactionHash}`);
-			hash = tx.transactionHash;
-			res.status(200).send(hash);
-		} catch (err) {
-			res.status(500).send(err);
-		}
-	}),
-);
+app.get('/api/dragon/create', async function createFunction(req, res, next) {
+	const { account, web3js } = loadGanacheAccount();
+	let hash = '';
+	try {
+		const tx = await createDragonToken(web3js, account, req.query.gas || 350000);
+		console.log(`Dragon created`);
+		console.log(`tx hash: ${tx.transactionHash}`);
+		hash = tx.transactionHash;
+		res.status(200).send(hash);
+	} catch (err) {
+		res.status(500).send(err);
+	}
+});
 
-app.post(
-	'/api/dragon/receive',
-	WAsync.wrapAsync(async function transferFunction(req, res, next) {
-		const { account, web3js } = loadGanacheAccount();
-		let hash = '';
-		let tx;
-		try {
-			for (const dragon of req.body.dragons) {
-				console.log(`Awaiting receiveDragonFromOracle with dragon ${JSON.stringify(dragon, null, 2)}`);
-				const receiverAddress = dragon.toMainchainAddress;
-				tx = await receiveDragonFromOracle(
-					web3js,
-					account,
-					req.query.gas || 350000,
-					dragon.uid,
-					dragon.data,
-					receiverAddress,
-				);
-			}
-			console.log(`tx hash: ${tx.transactionHash}`);
-			console.log('MENSAJE RECIBIDO', req.body);
-			hash = tx.transactionHash;
-			res.status(200).send(hash);
-		} catch (err) {
-			saveDragonOnOracle(req.body.dragons);
-			res.status(500).send(err);
-		}
-	}),
-);
-
-app.get(
-	'/api/dragon/transfer',
-	WAsync.wrapAsync(async function transferToSideFunction(req, res, next) {
-		const { account, web3js } = loadGanacheAccount();
-		try {
-			const data = await transferDragonToGateway(
+app.post('/api/dragon/receive', async function transferFunction(req, res, next) {
+	const { account, web3js } = loadGanacheAccount();
+	let hash = '';
+	let tx;
+	try {
+		for (const dragon of req.body.dragons) {
+			console.log(`Awaiting receiveDragonFromOracle with dragon ${JSON.stringify(dragon, null, 2)}`);
+			const receiverAddress = dragon.toMainchainAddress;
+			tx = await receiveDragonFromOracle(
 				web3js,
-				req.query.gas || 350000,
 				account,
-				req.query.id,
-				req.query.data,
+				req.query.gas || 350000,
+				dragon.uid,
+				dragon.data,
+				receiverAddress,
 			);
-			console.log(`\n Token with id ${req.query.id} was successfully transfered to gateway \n`);
-		} catch (err) {
-			console.log(err);
-			res.status(400).send(err);
 		}
-		res.status(200).send(`Token with id ${req.query.id} was successfully transfered to gateway`);
-	}),
-);
+		console.log(`tx hash: ${tx.transactionHash}`);
+		console.log('MENSAJE RECIBIDO', req.body);
+		hash = tx.transactionHash;
+		res.status(200).send(hash);
+	} catch (err) {
+		saveDragonOnOracle(req.body.dragons);
+		res.status(500).send(err);
+	}
+});
 
-app.get(
-	'/api/dragons',
-	WAsync.wrapAsync(async function getDragonFunction(req, res, next) {
-		const { account, web3js } = loadGanacheAccount();
-		try {
-			const data = await getMyDragons(web3js, account, req.query.gas || 350000);
-			console.log(`\nAddress ${account} holds dragons with id ${data}\n`);
+app.get('/api/dragon/transfer', async function transferToSideFunction(req, res, next) {
+	const { account, web3js } = loadGanacheAccount();
+	try {
+		const data = await transferDragonToGateway(web3js, req.query.gas || 350000, account, req.query.id, req.query.data);
+		console.log(`\n Token with id ${req.query.id} was successfully transfered to gateway \n`);
+	} catch (err) {
+		console.log(err);
+		res.status(400).send(err);
+	}
+	res.status(200).send(`Token with id ${req.query.id} was successfully transfered to gateway`);
+});
 
-			// @TODO para probar. Sacar
-			if (Array.isArray(data) && data.length > 0) {
-				for (dragonId in data) {
-					const dragonData = await getDragonDataById(web3js, account, dragonId);
-					console.log(`\n Data for dragon with id ${dragonId}`);
-					console.log(JSON.stringify(dragonData, null, 2));
-				}
+app.get('/api/dragons', async function getDragonFunction(req, res, next) {
+	const { account, web3js } = loadGanacheAccount();
+	try {
+		const data = await getMyDragons(web3js, account, req.query.gas || 350000);
+		console.log(`\nAddress ${account} holds dragons with id ${data}\n`);
+
+		// @TODO para probar. Sacar
+		if (Array.isArray(data) && data.length > 0) {
+			for (dragonId in data) {
+				const dragonData = await getDragonDataById(web3js, account, dragonId);
+				console.log(`\n Data for dragon with id ${dragonId}`);
+				console.log(JSON.stringify(dragonData, null, 2));
 			}
-
-			res.status(200).send(data);
-		} catch (err) {
-			console.log(`Error getting dragons data:${err}`);
-			res.status(500).send(err);
 		}
-	}),
-);
 
-app.get(
-	'/api/dragon',
-	WAsync.wrapAsync(async function getDragonFunction(req, res, next) {
-		const { account, web3js } = loadGanacheAccount();
-		try {
-			const data = await getDragonDataById(web3js, account, req.query.id);
-			data.sname = web3js.utils.toUtf8(data.name);
-			res.status(200).send(data);
-		} catch (err) {
-			console.log(`Error getting dragon data with id: ${req.query.id}`);
-			res.status(500).send(err);
-		}
-	}),
-);
+		res.status(200).send(data);
+	} catch (err) {
+		console.log(`Error getting dragons data:${err}`);
+		res.status(500).send(err);
+	}
+});
 
-app.get(
-	'/api/mapAccount',
-	WAsync.wrapAsync(async function getMapFunction(req, res, next) {
-		const { account, web3js } = loadGanacheAccount(req.query.account);
-		try {
-			await mapAccount(web3js, account, req.query.gas || 350000, req.query.sideAccount);
-			res.status(200).send('OK');
-		} catch (err) {
-			console.log(`Error mapping mainchain to sidechain ${err}`);
-			res.status(400).send(err);
-		}
-	}),
-);
+app.get('/api/dragon', async function getDragonFunction(req, res, next) {
+	const { account, web3js } = loadGanacheAccount();
+	try {
+		const data = await getDragonDataById(web3js, account, req.query.id);
+		data.sname = web3js.utils.toUtf8(data.name);
+		res.status(200).send(data);
+	} catch (err) {
+		console.log(`Error getting dragon data with id: ${req.query.id}`);
+		res.status(500).send(err);
+	}
+});
+
+app.get('/api/mapAccount', async function getMapFunction(req, res, next) {
+	const { account, web3js } = loadGanacheAccount(req.query.account);
+	try {
+		await mapAccount(web3js, account, req.query.gas || 350000, req.query.sideAccount);
+		res.status(200).send('OK');
+	} catch (err) {
+		console.log(`Error mapping mainchain to sidechain ${err}`);
+		res.status(400).send(err);
+	}
+});
 
 const PORT = 8002;
 http.createServer(app).listen(PORT, () => {
--- truffle-project/sidechain-cli.js
+++ truffle-project/sidechain-cli.js
@@ -1,4 +1,3 @@
-const WAsync = require('@rimiti/express-async');
 const Web3 = require('web3');
 const fs = require('fs');
 const path = require('path');
@@ -49,125 +48,107 @@ app.get('/', (req, res) => {
 	res.status(200).send('Welcome to API REST');
 });
 
-app.get(
-	'/api/dragon/create',
-	WAsync.wrapAsync(async function createFunction(req, res, next) {
-		const { account, web3js, client } = loadLoomAccount(req.query.account);
+app.get('/api/dragon/create', async function createFunction(req, res, next) {
+	const { account, web3js, client } = loadLoomAccount(req.query.account);
 
-		let hash = '';
-		try {
-			const tx = await createDragonToken(web3js, account, req.query.gas || 350000);
-			console.log(`Dragon created, owner account:`, account);
-			console.log(`tx hash: ${tx.transactionHash}`);
-			hash = tx.transactionHash;
-			res.status(200).send(hash);
-		} catch (err) {
-			res.status(400).send(err);
-		} finally {
-			if (client) client.disconnect();
-		}
-	}),
-);
+	let hash = '';
+	try {
+		const tx = await createDragonToken(web3js, account, req.query.gas || 350000);
+		console.log(`Dragon created, owner account:`, account);
+		console.log(`tx hash: ${tx.transactionHash}`);
+		hash = tx.transactionHash;
+		res.status(200).send(hash);
+	} catch (err) {
+		res.status(400).send(err);
+	} finally {
+		if (client) client.disconnect();
+	}
+});
 
-app.post(
-	'/api/dragon/receive',
-	WAsync.wrapAsync(async function transferFunction(req, res, next) {
-		const { account, web3js, client } = loadLoomAccount();
-		let hash = '';
-		let tx;
-		try {
-			for (const dragon of req.body.dragons) {
-				console.log(`Awaiting receiveDragonFromOracle with dragon ${JSON.stringify(dragon, null, 2)}`);
-				const receiverAddress = dragon.toSidechainAddress;
-				tx = await receiveDragonFromOracle(
-					web3js,
-					account,
-					req.query.gas || 350000,
-					dragon.uid,
-					dragon.data,
-					receiverAddress,
-				);
-			}
-			console.log(`tx hash: ${tx.transactionHash}`);
-			console.log('MENSAJE RECIBIDO', req.body.dragons);
-			hash = tx.transactionHash;
-			res.status(200).send(hash);
-		} catch (err) {
-			saveDragonOnOracle(req.body.dragons);
-			res.status(500).send(err);
-		} finally {
-			if (client) client.disconnect();
+app.post('/api/dragon/receive', async function transferFunction(req, res, next) {
+	const { account, web3js, client } = loadLoomAccount();
+	let hash = '';
+	let tx;
+	try {
+		for (const dragon of req.body.dragons) {
+			console.log(`Awaiting receiveDragonFromOracle with dragon ${JSON.stringify(dragon, null, 2)}`);
+			const receiverAddress = dragon.toSidechainAddress;
+			tx = await receiveDragonFromOracle(
+				web3js,
+				account,
+				req.query.gas || 350000,
+				dragon.uid,
+				dragon.data,
+				receiverAddress,
+			);
 		}
-	}),
-);
+		console.log(`tx hash: ${tx.transactionHash}`);
+		console.log('MENSAJE RECIBIDO', req.body.dragons);
+		hash = tx.transactionHash;
+		res.status(200).send(hash);
+	} catch (err) {
+		saveDragonOnOracle(req.body.dragons);
+		res.status(500).send(err);
+	} finally {
+		if (client) client.disconnect();
+	}
+});
 
-app.get(
-	'/api/dragon/transfer',
-	WAsync.wrapAsync(async function transferFunction(req, res, next) {
-		const { account, web3js, client } = loadLoomAccount(req.query.account);
-		try {
-			const data = await transferDragonToGateway(web3js, req.query.gas || 350000, account, req.query.id);
-			console.log(`\n Token with id ${req.query.id} was successfully transfered to gateway \n`);
-			res.status(200).send(`Token with id ${req.query.id} was successfully transfered to gateway`);
-		} catch (err) {
-			res.status(400).send(err);
-		} finally {
-			if (client) client.disconnect();
-		}
-	}),
-);
+app.get('/api/dragon/transfer', async function transferFunction(req, res, next) {
+	const { account, web3js, client } = loadLoomAccount(req.query.account);
+	try {
+		const data = await transferDragonToGateway(web3js, req.query.gas || 350000, account, req.query.id);
+		console.log(`\n Token with id ${req.query.id} was successfully transfered to gateway \n`);
+		res.status(200).send(`Token with id ${req.query.id} was successfully transfered to gateway`);
+	} catch (err) {
+		res.status(400).send(err);
+	} finally {
+		if (client) client.disconnect();
+	}
+});
 
-app.get(
-	'/api/dragons',
-	WAsync.wrapAsync(async function getDragonFunction(req, res, next) {
-		const { account, web3js, client } = loadLoomAccount(req.query.account);
-		let data = '';
-		try {
-			data = await getMyDragons(web3js, account, req.query.gas || 350000);
-			console.log(`\nAddress ${account} holds dragons with id ${data}\n`);
-			res.status(200).send(data);
-		} catch (err) {
-			console.log(`Error getting dragons data:${err}`);
-			res.status(500).send(err);
-		} finally {
-			if (client) client.disconnect();
-		}
-	}),
-);
+app.get('/api/dragons', async function getDragonFunction(req, res, next) {
+	const { account, web3js, client } = loadLoomAccount(req.query.account);
+	let data = '';
+	try {
+		data = await getMyDragons(web3js, account, req.query.gas || 350000);
+		console.log(`\nAddress ${account} holds dragons with id ${data}\n`);
+		res.status(200).send(data);
+	} catch (err) {
+		console.log(`Error getting dragons data:${err}`);
+		res.status(500).send(err);
+	} finally {
+		if (client) client.disconnect();
+	}
+});
 
-app.get(
-	'/api/mapAccount',
-	WAsync.wrapAsync(async function getMapFunction(req, res, next) {
-		const { account, web3js, client } = loadLoomAccount(req.query.account);
-		try {
-			await mapAccount(web3js, account, req.query.gas || 350000, req.query.mainAccount);
-			res.status(200).send('OK');
-		} catch (err) {
-			console.log(`Error mapping sidechain to mainchain ${err}`);
-			res.status(400).send(err);
-		} finally {
-			if (client) client.disconnect();
-		}
-	}),
-);
+app.get('/api/mapAccount', async function getMapFunction(req, res, next) {
+	const { account, web3js, client } = loadLoomAccount(req.query.account);
+	try {
+		await mapAccount(web3js, account, req.query.gas || 350000, req.query.mainAccount);
+		res.status(200).send('OK');
+	} catch (err) {
+		console.log(`Error mapping sidechain to mainchain ${err}`);
+		res.status(400).send(err);
+	} finally {
+		if (client) client.disconnect();
+	}
+});
 
-app.get(
-	'/api/dragon',
-	WAsync.wrapAsync(async function getMapFunction(req, res, next) {
-		// getDragonDataById(web3js, ownerAccount, dragonId) {
-		const { account, web3js, client } = loadLoomAccount(req.query.account);
-		try {
-			const data = await getDragonDataById(web3js, account, req.query.id);
-			data.sname = web3js.utils.toUtf8(data.name);
-			res.status(200).send(data);
-		} catch (err) {
-			console.log(`Error getting dragon with id: ${req.id}`);
-			res.status(400).send(err);
-		} finally {
-			if (client) client.disconnect();
-		}
-	}),
-);
+app.get('/api/dragon', async function getMapFunction(req, res, next) {
+	// getDragonDataById(web3js, ownerAccount, dragonId) {
+	const { account, web3js, client } = loadLoomAccount(req.query.account);
+	try {
+		const data = await getDragonDataById(web3js, account, req.query.id);
+		data.sname = web3js.utils.toUtf8(data.name);
+		res.status(200).send(data);
+	} catch (err) {
+		console.log(`Error getting dragon with id: ${req.id}`);
+		res.status(400).send(err);
+	} finally {
+		if (client) client.disconnect();
+	}
+});
 
 http.createServer(app).listen(8001, () => {
 	console.log('Server started at http://localhost:8001');

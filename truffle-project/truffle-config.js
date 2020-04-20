568ca318c79d57c58412e65d19694feb28d177f7
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2020-04-19 22:55:15 -0300

    'process.env.MAINCHAIN_URL' for bfa in truffleConfig

--- truffle-project/truffle-config.js
+++ truffle-project/truffle-config.js
@@ -77,10 +77,11 @@ module.exports = {
 		},
 		bfa: {
 			provider() {
+				const urlKeyProvider = `http://${process.env.MAINCHAIN_URL}:8545/`;
 				// create provider using the private key of the prefunded bfa account 0x28863498efede12296888f7ca6cf0b94974fbdbc
 				return new PrivateKeyProvider(
 					'dff874fa1f53c713f31b5831c25fe56657808bd0b379a7f28442af8a6de79cb2',
-					'http://127.0.0.1:8545/',
+					urlKeyProvider,
 				);
 			},
 			network_id: '12345',

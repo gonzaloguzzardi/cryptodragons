1226826ee819762103277d43c044eed44d7cbf05
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2020-07-11 19:43:12 -0300

    Docker compose not launching clis anymore

--- oracle/oracle.js
+++ oracle/oracle.js
@@ -32,6 +32,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// LISTENING BLOCKCHAINS
 listenMainChainEvents();
 listenSideChainEvents();
 
--- truffle-project/src/services/index.js
+++ truffle-project/src/services/index.js
@@ -6,13 +6,10 @@ const {
 	_isMap,
 } = require('./commons');
 
-const { saveDragonOnOracle } = require('./oracle-actions');
-
 module.exports = {
 	_sMapAccountSideChain,
 	_sMapAccountMainChain,
 	_sTransferDragonToGateway,
 	_sReceiveDragonFromOracle,
-	saveDragonOnOracle,
 	_isMap,
 };

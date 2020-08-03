17db21b903469038d3a8f9fb64476e9a69987bf2
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2020-08-02 21:04:08 -0300

    Logs of error in receiving from oracle

--- oracle/services/collect-from-mainchain-gateway-and-send-to-sidechain.js
+++ oracle/services/collect-from-mainchain-gateway-and-send-to-sidechain.js
@@ -10,7 +10,20 @@ function transferDragonsToSide(dragons) {
       const receiverAddress = dragon.toSidechainAddress;
       const dragonId = dragon.uid;
       const dragonData = dragon.data;
-
+        // Con este hardcode para senderAddress funciona, pero revisar que onda
+        // REVISAR
+				// senderAddress:  0xfee39fad945754831b59b92a1a8339f65358792d
+        // dragon.from:  0x28863498efeDE12296888F7CA6Cf0B94974fBdbc
+        // {
+        //   '0': '0x28863498efeDE12296888F7CA6Cf0B94974fBdbc',
+        //   '1': '1',
+        //   '2': '0xfeE39FAD945754831B59b92A1a8339F65358792d',
+        //   '3': '0x7ede1c64030000000000000000000000000000000000000000000000000000007465737420647261676f6e0000000000000000000000000000000000000000000100000000000000020000000200000000000000000032000700070007003c00ff',
+        //   from: '0x28863498efeDE12296888F7CA6Cf0B94974fBdbc',
+        //   uid: '1',
+        //   toSidechainAddress: '0xfeE39FAD945754831B59b92A1a8339F65358792d',
+        //   data: '0x7ede1c64030000000000000000000000000000000000000000000000000000007465737420647261676f6e0000000000000000000000000000000000000000000100000000000000020000000200000000000000000032000700070007003c00ff'
+        // }
       sideChainGatewayInstance.methods
         .receiveDragon(receiverAddress, dragonId, dragonData)
         .send({ from: senderAddress, gas: 350000 });
--- oracle/services/collect-from-sidechain-gateway-and-send-to-mainchain.js
+++ oracle/services/collect-from-sidechain-gateway-and-send-to-mainchain.js
@@ -12,6 +12,20 @@ function transferDragonsToMain(dragons) {
 			const dragonData = dragon.data;
 
 			try {
+					// Con este hardcode para senderAddress funciona, pero revisar que onda
+					// REVISAR
+					// senderAddress:  0x28863498efede12296888f7ca6cf0b94974fbdbc
+					// dragon.from:  0xfeE39FAD945754831B59b92A1a8339F65358792d
+					// {
+					// 	'0': '0xfeE39FAD945754831B59b92A1a8339F65358792d',
+					// 	'1': '1',
+					// 	'2': '0x28863498efeDE12296888F7CA6Cf0B94974fBdbc',
+					// 	'3': '0x7ede1c64030000000000000000000000000000000000000000000000000000007465737420647261676f6e0000000000000000000000000000000000000000000100000000000000020000000200000000000000000032000700070007003c00ff',
+					// 	from: '0xfeE39FAD945754831B59b92A1a8339F65358792d',
+					// 	uid: '1',
+					// 	toMainchainAddress: '0x28863498efeDE12296888F7CA6Cf0B94974fBdbc',
+					// 	data: '0x7ede1c64030000000000000000000000000000000000000000000000000000007465737420647261676f6e0000000000000000000000000000000000000000000100000000000000020000000200000000000000000032000700070007003c00ff'
+					// }
 				mainChainGatewayInstance.methods
 					.receiveDragon(receiverAddress, dragonId, dragonData)
 					.send({ from: senderAddress, gas: 450000 });

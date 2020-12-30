5cbd2a73d8fb3855f5e91a6136446f8e60d600f3
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2020-12-30 15:20:48 -0300

    Prettier

--- web/services/blockchain-interaction/mainchain/client-factory.js
+++ web/services/blockchain-interaction/mainchain/client-factory.js
@@ -1,35 +1,38 @@
 /* eslint-disable no-undef */
-import Web3 from 'web3';
-const bfaAddress = !process.env.DOCKERENV ? 'http://127.0.0.1:8545' : 'http://bfa:8545';
-const web3js = new Web3(new Web3.providers.HttpProvider(bfaAddress));
+import Web3 from 'web3'
+const bfaAddress = !process.env.DOCKERENV ? 'http://127.0.0.1:8545' : 'http://bfa:8545'
+const web3js = new Web3(new Web3.providers.HttpProvider(bfaAddress))
 
 async function getMainNetTokenContract() {
-  const networkId = await web3js.eth.net.getId();
-  const MainchainDragonTokenJson = require('../../../contracts/MainnetTransferableDragon.json');
-	return new web3js.eth.Contract(MainchainDragonTokenJson.abi, MainchainDragonTokenJson.networks[networkId].address);
+  const networkId = await web3js.eth.net.getId()
+  const MainchainDragonTokenJson = require('../../../contracts/MainnetTransferableDragon.json')
+  return new web3js.eth.Contract(
+    MainchainDragonTokenJson.abi,
+    MainchainDragonTokenJson.networks[networkId].address
+  )
 }
 
 async function getMainNetGatewayContract() {
-  const networkId = await web3js.eth.net.getId();
-  const GatewayJson = require('../../../contracts/MainnetGateway.json');
-	return new web3js.eth.Contract(GatewayJson.abi, GatewayJson.networks[networkId].address);
+  const networkId = await web3js.eth.net.getId()
+  const GatewayJson = require('../../../contracts/MainnetGateway.json')
+  return new web3js.eth.Contract(GatewayJson.abi, GatewayJson.networks[networkId].address)
 }
 
 export default async function clientFactory() {
-  if (!window || !window.ethereum) return null;
+  if (!window || !window.ethereum) return null
 
   return Promise.all([
     web3js.eth.getAccounts(),
     web3js.eth.net.getId(),
     getMainNetTokenContract(),
     getMainNetGatewayContract(),
-  ]).then(values => ({
+  ])
+    .then((values) => ({
       web3js,
       account: values[0][0],
       netId: values[1],
       tokenContract: values[2],
       gatewayContract: values[3],
-    })
-  ).catch(err => console.error(err));
-
+    }))
+    .catch((err) => console.error(err))
 }
--- web/services/blockchain-interaction/sidechain/client-factory.js
+++ web/services/blockchain-interaction/sidechain/client-factory.js
@@ -1,55 +1,66 @@
-import Web3 from 'web3';
-import { Client, NonceTxMiddleware, SignedTxMiddleware, LocalAddress, CryptoUtils, LoomProvider } from 'loom-js';
+import Web3 from 'web3'
+import {
+  Client,
+  NonceTxMiddleware,
+  SignedTxMiddleware,
+  LocalAddress,
+  CryptoUtils,
+  LoomProvider,
+} from 'loom-js'
 
-const DappchainDragonTokenJson = require('../../../contracts/DappchainTransferableDragon.json');
-const GatewayJson = require('../../../contracts/DappchainGateway');
+const DappchainDragonTokenJson = require('../../../contracts/DappchainTransferableDragon.json')
+const GatewayJson = require('../../../contracts/DappchainGateway')
 
-const loomChainId = '13654820909954'; // TODO ver si cambia o si es siempre el mismo
+const loomChainId = '13654820909954' // TODO ver si cambia o si es siempre el mismo
 
 async function getLoomTokenContract(web3js) {
-	return new web3js.eth.Contract(DappchainDragonTokenJson.abi, DappchainDragonTokenJson.networks[loomChainId].address);
+  return new web3js.eth.Contract(
+    DappchainDragonTokenJson.abi,
+    DappchainDragonTokenJson.networks[loomChainId].address
+  )
 }
 
 async function getLoomGatewayContract(web3js) {
-	return new web3js.eth.Contract(GatewayJson.abi, GatewayJson.networks[loomChainId].address);
+  return new web3js.eth.Contract(GatewayJson.abi, GatewayJson.networks[loomChainId].address)
 }
 
 async function loadLoomAccount() {
   // const accountPath = './misc/loom_private_key';
   // const privateKeyStr = fs.readFileSync(path.join(__dirname, accountPath), 'utf-8');
-  const privateKeyStr = "OwM4hj6RcjBecJSrObLjyB4R/5dbQFk0ZpAyrIn7kYAFsuBAV7RoOIbw9V3tGB9I2WodYtN373D46UHn3EtgqQ==";
-  const privateKey = CryptoUtils.B64ToUint8Array(privateKeyStr);
-  const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey);
-  const loomAddress = !process.env.DOCKERENV ? 'ws://127.0.0.1:46658' : 'ws://loom:46658';
-  const client = new Client('default', `${loomAddress}/websocket`, `${loomAddress}/queryws`);
-  client.txMiddleware = [new NonceTxMiddleware(publicKey, client), new SignedTxMiddleware(privateKey)];
+  const privateKeyStr =
+    'OwM4hj6RcjBecJSrObLjyB4R/5dbQFk0ZpAyrIn7kYAFsuBAV7RoOIbw9V3tGB9I2WodYtN373D46UHn3EtgqQ=='
+  const privateKey = CryptoUtils.B64ToUint8Array(privateKeyStr)
+  const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey)
+  const loomAddress = !process.env.DOCKERENV ? 'ws://127.0.0.1:46658' : 'ws://loom:46658'
+  const client = new Client('default', `${loomAddress}/websocket`, `${loomAddress}/queryws`)
+  client.txMiddleware = [
+    new NonceTxMiddleware(publicKey, client),
+    new SignedTxMiddleware(privateKey),
+  ]
   client.on('error', (msg) => {
-    console.error('Loom connection error', msg);
-  });
+    console.error('Loom connection error', msg)
+  })
 
   return {
     account: LocalAddress.fromPublicKey(publicKey).toString(),
     web3js: new Web3(new LoomProvider(client, privateKey)),
     client,
-  };
+  }
 }
 
 export default async function clientFactory() {
   return loadLoomAccount()
-    .then(({ account, web3js, client }) => (
-      Promise.all([
-        getLoomTokenContract(web3js),
-        getLoomGatewayContract(web3js),
-      ])
-        .then(values => ({
+    .then(({ account, web3js, client }) =>
+      Promise.all([getLoomTokenContract(web3js), getLoomGatewayContract(web3js)]).then(
+        (values) => ({
           account,
           web3js,
           loomClient: client,
           netId: loomChainId,
           tokenContract: values[0],
           gatewayContract: values[1],
-        }))
-    ))
-    .catch(err => console.error(err));
-
-};
+        })
+      )
+    )
+    .catch((err) => console.error(err))
+}

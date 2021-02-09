53743fb81313059ac85bf5b695954aaa2a9a5fbf
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2021-02-09 14:52:25 -0300

    Marketplace collections card init

--- web/components/home/marketplace/desktop.tsx
+++ web/components/home/marketplace/desktop.tsx
@@ -4,21 +4,25 @@ import { ReactElement } from 'react'
 
 import styles from './desktop.module.scss'
 
-// import ShowcaseCardDemo from '../../card/cards-stacked/showcase-card-demo'
+import MarketplaceCollections from '../../card/cards-stacked/desktop'
 
 export default function MarketplaceSection(): ReactElement {
   return (
     <div className={styles.main}>
-      <div className={styles.title}>
-        <Typography variant="h3">Start your own collection</Typography>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <Typography variant="h3">Start your own collection</Typography>
+        </div>
+        <div className={styles.subtitle}>
+          <Typography variant="subtitle1" color="textSecondary">
+            In CryptoDragons you can collect Dragons of all colours and shapes. Create Collections
+            of your favourite dragons and share them with our cryptodragons community.
+          </Typography>
+        </div>
+        <div className={styles.marketplaceCollections}>
+          <MarketplaceCollections />
+        </div>
       </div>
-      <div className={styles.subtitle}>
-        <Typography variant="subtitle1" color="textSecondary">
-          In CryptoDragons you can collect Dragons of all colours and shapes. Create Collections of
-          your favourite dragons and share them with our cryptodragons community.
-        </Typography>
-      </div>
-      {/* <ShowcaseCardDemo /> */}
     </div>
   )
 }

919b4c185ae47dbc5c3a74e57193ba2c9bddb823
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2021-02-09 18:15:38 -0300

    4 cards

--- web/components/card/cards-stacked/desktop.tsx
+++ web/components/card/cards-stacked/desktop.tsx
@@ -3,6 +3,7 @@ import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
+import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import Avatar from '@material-ui/core/Avatar'
 import { ReactElement } from 'react'
@@ -11,30 +12,115 @@ import styles from './desktop.module.scss'
 
 export default function ShowcaseCardDemo(): ReactElement {
   return (
-    <Card className={styles.card}>
-      <CardActionArea>
-        <CardContent>
-          <Typography variant="h5" component="h2">
-            Lizard
-          </Typography>
-        </CardContent>
-        <CardMedia className={styles.media} image="/assets/dragonsito2.jpeg" title="Dragon 1" />
-        <CardContent>
-          <div className={styles.cardBottomContent}>
-            <div className={styles.cardBottomContentAvatar}>
-              <Avatar alt="Mocca" src="/assets/mocca.jpg" />
-            </div>
-            <div className={styles.cardBottomContentText}>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Owned by
+    <Grid container justify="space-between">
+      <Grid container item justify="center" xs={3}>
+        <Card className={styles.card}>
+          <CardActionArea>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Lizard
               </Typography>
-              <Typography variant="body2" color="textPrimary" component="p">
-                Mocca
+            </CardContent>
+            <CardMedia className={styles.media} image="/assets/dragonsito2.jpeg" title="Dragon 1" />
+            <CardContent>
+              <div className={styles.cardBottomContent}>
+                <div className={styles.cardBottomContentAvatar}>
+                  <Avatar alt="Mocca" src="/assets/mocca.jpg" />
+                </div>
+                <div className={styles.cardBottomContentText}>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Owned by
+                  </Typography>
+                  <Typography variant="body2" color="textPrimary" component="p">
+                    Mocca
+                  </Typography>
+                </div>
+              </div>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid container item justify="center" xs={3}>
+        <Card className={styles.card}>
+          <CardActionArea>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Lizard
               </Typography>
-            </div>
-          </div>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+            </CardContent>
+            <CardMedia className={styles.media} image="/assets/dragonsito2.jpeg" title="Dragon 1" />
+            <CardContent>
+              <div className={styles.cardBottomContent}>
+                <div className={styles.cardBottomContentAvatar}>
+                  <Avatar alt="Mocca" src="/assets/mocca.jpg" />
+                </div>
+                <div className={styles.cardBottomContentText}>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Owned by
+                  </Typography>
+                  <Typography variant="body2" color="textPrimary" component="p">
+                    Mocca
+                  </Typography>
+                </div>
+              </div>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid container item justify="center" xs={3}>
+        <Card className={styles.card}>
+          <CardActionArea>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Lizard
+              </Typography>
+            </CardContent>
+            <CardMedia className={styles.media} image="/assets/dragonsito2.jpeg" title="Dragon 1" />
+            <CardContent>
+              <div className={styles.cardBottomContent}>
+                <div className={styles.cardBottomContentAvatar}>
+                  <Avatar alt="Mocca" src="/assets/mocca.jpg" />
+                </div>
+                <div className={styles.cardBottomContentText}>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Owned by
+                  </Typography>
+                  <Typography variant="body2" color="textPrimary" component="p">
+                    Mocca
+                  </Typography>
+                </div>
+              </div>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid container item justify="center" xs={3}>
+        <Card className={styles.card}>
+          <CardActionArea>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Lizard
+              </Typography>
+            </CardContent>
+            <CardMedia className={styles.media} image="/assets/dragonsito2.jpeg" title="Dragon 1" />
+            <CardContent>
+              <div className={styles.cardBottomContent}>
+                <div className={styles.cardBottomContentAvatar}>
+                  <Avatar alt="Mocca" src="/assets/mocca.jpg" />
+                </div>
+                <div className={styles.cardBottomContentText}>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Owned by
+                  </Typography>
+                  <Typography variant="body2" color="textPrimary" component="p">
+                    Mocca
+                  </Typography>
+                </div>
+              </div>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }

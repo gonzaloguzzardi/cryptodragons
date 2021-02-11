a9e9d8f55d40b157ce785f068a7d6cb0f5ba8d47
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2021-02-11 16:07:24 -0300

    Home parallax

--- web/components/home/landing-section/desktop.tsx
+++ web/components/home/landing-section/desktop.tsx
@@ -1,3 +1,4 @@
+import { Parallax } from 'react-parallax'
 import Typography from '@material-ui/core/Typography'
 
 import { ReactElement } from 'react'
@@ -6,13 +7,20 @@ import styles from './desktop.module.scss'
 
 export default function LandingSection(): ReactElement {
   return (
-    <div className={styles.main}>
-      <div className={styles.title}>
-        <Typography variant="h2">CryptoDragons</Typography>
+    <Parallax
+      blur={0}
+      bgImage="/assets/home/background_dragon-land.jpg"
+      bgImageAlt="background landing"
+      strength={100}
+    >
+      <div className={styles.main}>
+        <div className={styles.title}>
+          <Typography variant="h2">CryptoDragons</Typography>
+        </div>
+        <div className={styles.images}>
+          <img src="/assets/home/dragon-1.png" alt="" width="400px" height="400px" />
+        </div>
       </div>
-      <div className={styles.images}>
-        <img src="/assets/home/dragon-1.png" alt="" width="400px" height="400px" />
-      </div>
-    </div>
+    </Parallax>
   )
 }

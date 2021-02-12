fab5a4f5804bdc788731e9d4c718fdf210448fca
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2021-02-12 18:17:50 -0300

    Update snapshots

--- web/containers/home/__tests__/index.test.tsx
+++ web/containers/home/__tests__/index.test.tsx
@@ -3,7 +3,7 @@ import IndexPage from '../'
 
 describe('Index page', () => {
   it('should match the snapshot', () => {
-    const tree = renderer.create(<IndexPage deviceType="mobile" />).toJSON()
+    const tree = renderer.create(<IndexPage deviceType="desktop" />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })

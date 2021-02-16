c457479d15f664a016225580212e4bcf051c35ff
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2021-02-16 04:34:31 -0300

    added publish method in dragon token contract

--- truffle-project/contracts/mainnet/MainnetMerchantableDragon.sol
+++ truffle-project/contracts/mainnet/MainnetMerchantableDragon.sol
@@ -2,6 +2,14 @@ pragma solidity ^0.5.0;
 
 import "./MainnetTransferableDragon.sol";
 
+contract IMarketplace {
+    function createOrder (
+        uint256 dragonId,
+        string memory title,
+		string memory description,
+		uint256 price external;
+}
+
 contract MainnetMerchantableDragon is MainnetTransferableDragon {
 
     address private marketplaceAddress;
@@ -11,5 +19,19 @@ contract MainnetMerchantableDragon is MainnetTransferableDragon {
     function setMarketplace(address _marketplaceAddress) external onlyOwner {
         require(_marketplaceAddress != address(0), 'The marketplace address cannot be empty');
         marketplaceAddress = _marketplaceAddress;
+    }
+
+    function publishDragon(
+        uint256 _tokenId, 
+        string memory _title, 
+        string memory _description,
+        uint256 _price) external onlyDragonOwner(_tokenId) {
+
+        require(marketplaceAddress != address(0), 'The marketplace address needs to be initialized');
+
+        // Allow marketplace to transfer ownership
+        approve(marketplaceAddress, _tokenId);
+
+        IMarketplace(marketplaceAddress).createOrder(_tokenId, _title, _description, _price)
     }
 }

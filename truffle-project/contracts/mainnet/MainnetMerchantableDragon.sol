1f66742d2ebba19df6b1d4adafabb3a8c4e9f30d
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2021-02-17 03:48:06 -0300

    added buy dragon function

--- truffle-project/contracts/mainnet/MainnetMerchantableDragon.sol
+++ truffle-project/contracts/mainnet/MainnetMerchantableDragon.sol
@@ -1,40 +1,40 @@
 pragma solidity ^0.5.0;
 
-import "./MainnetTransferableDragon.sol";
+import './MainnetTransferableDragon.sol';
 
 contract IMarketplace {
-    function createSellOrder (
-        uint256 dragonId,
-        string memory title,
-		string memory description,
-		uint256 price external;
+	function createSellOrder(
+		uint256 dragonId,
+		string calldata title,
+		string calldata description,
+		uint256 price
+	) external;
 }
 
 contract MainnetMerchantableDragon is MainnetTransferableDragon {
+	address private marketplaceAddress;
 
-    address private marketplaceAddress;
+	constructor(address gateway, uint8 blockchainId) public MainnetTransferableDragon(gateway, blockchainId) {}
 
-    constructor(address gateway, uint8 blockchainId) MainnetTransferableDragon(gateway, blockchainId) public {}
+	function setMarketplace(address _marketplaceAddress) external onlyOwner {
+		require(_marketplaceAddress != address(0), 'The marketplace address cannot be empty');
+		marketplaceAddress = _marketplaceAddress;
+	}
 
-    function setMarketplace(address _marketplaceAddress) external onlyOwner {
-        require(_marketplaceAddress != address(0), 'The marketplace address cannot be empty');
-        marketplaceAddress = _marketplaceAddress;
-    }
-
-    function publishDragon(
-        uint256 _tokenId, 
-        string memory _title, 
-        string memory _description,
-        uint256 _price) external onlyDragonOwner(_tokenId) {
-
-        require(marketplaceAddress != address(0), 'The marketplace address needs to be initialized');
-        require(bytes(_title).length > 0, 'The title cannot be empty');
+	function publishDragon(
+		uint256 _tokenId,
+		string calldata _title,
+		string calldata _description,
+		uint256 _price
+	) external onlyDragonOwner(_tokenId) {
+		require(marketplaceAddress != address(0), 'The marketplace address needs to be initialized');
+		require(bytes(_title).length > 0, 'The title cannot be empty');
 		require(bytes(_description).length > 0, 'The description cannot be empty');
 		require(_price > 0, 'The price cannot be empty');
 
-        // Allow marketplace to transfer ownership
-        approve(marketplaceAddress, _tokenId);
+		// Allow marketplace to transfer ownership
+		approve(marketplaceAddress, _tokenId);
 
-        IMarketplace(marketplaceAddress).createSellOrder(_tokenId, _title, _description, _price);
-    }
+		IMarketplace(marketplaceAddress).createSellOrder(_tokenId, _title, _description, _price);
+	}
 }
--- truffle-project/contracts/mainnet/marketplace/MainnetMarketplace.sol
+++ truffle-project/contracts/mainnet/marketplace/MainnetMarketplace.sol
@@ -1,29 +1,26 @@
 pragma solidity ^0.5.0;
 
+contract IERC721 {
+	function safeTransferFrom(
+		address from,
+		address to,
+		uint256 tokenId
+	) external;
+}
+
 contract MainnetMarketplace {
-	/** Seller address => products
-    The products waiting to be fulfilled by the seller, used by sellers to check 
-    which orders have to be filled
-    */
-	mapping(address => Order[]) public pendingSellerOrders;
+	// dragon id => sell order
+	mapping(uint256 => SellOrder) public sellOrderById;
 
-	/** Buyer address => products
-    The products that the buyer purchased waiting to be sent
-    */
-	mapping(address => Order[]) public pendingBuyerOrders;
-	mapping(address => Order[]) public completedOrders;
+	// Contain sell orders for dragons on sale
+	SellOrder[] public sellOrders;
 
-	// Product id => product
-	mapping(uint256 => Product) public productById;
+	// History containing all sales
+	Sale[] public salesHistory;
 
-	// Product id => order
-	mapping(uint256 => Order) public orderById;
-
-	Product[] public products;
-	uint256 public lastId;
 	address public dragonTokenAddress;
 
-	struct Product {
+	struct SellOrder {
 		uint256 id;
 		string title;
 		string description;
@@ -31,18 +28,12 @@ contract MainnetMarketplace {
 		address payable owner;
 		uint256 price;
 	}
-	struct Order {
+	struct Sale {
 		uint256 id;
+		address owner;
 		address buyer;
-		string nameSurname;
-		string lineOneDirection;
-		string lineTwoDirection;
-		bytes32 city;
-		bytes32 stateRegion;
-		uint256 postalCode;
-		bytes32 country;
-		uint256 phone;
-		string state; // Either 'pending', 'completed'
+		uint256 price;
+		string title;
 	}
 
 	/// @notice To setup the address of the ERC-721 token to use for this contract
@@ -56,108 +47,59 @@ contract MainnetMarketplace {
 	 * @dev Throws if function isn't called from dragon contract
 	 */
 	modifier onlyFromDragonToken() {
-		require(ownerOf(_dragonTokenAddress) == msg.sender, 'Only dragon token can call this function');
+		require(dragonTokenAddress == msg.sender, 'Only dragon token can call this function');
 		_;
 	}
 
-	function createOrder(
+	/// @notice To publish a sellOrder as a seller
+	/// @param _dragonId The if of the dragon being published
+	/// @param _title The title of the sellOrder
+	/// @param _description The description of the sellOrder
+	/// @param _price The price of the sellOrder in ETH
+	function createSellOrder(
 		uint256 _dragonId,
-		string memory _title,
-		string memory _description,
+		string calldata _title,
+		string calldata _description,
 		uint256 _price
-	) public onlyFromDragonToken() {
+	) external onlyFromDragonToken() {
 		// Parameter validity are checked in dragon token contract
-		Product memory p = Product(lastId, _title, _description, now, msg.sender, _price);
-		products.push(p);
-		productById[lastId] = p;
+		SellOrder memory sellOrder = SellOrder(_dragonId, _title, _description, now, msg.sender, _price);
+		sellOrders.push(sellOrder);
+		sellOrderById[_dragonId] = sellOrder;
 	}
 
-	// TODO en lugar de mint tiene que pasarle el id, chequear que quien trasnfiere sea dueño del dragon, y hacer la transferencia de dominio al marketplace referenciando al dueño
+	/// @notice To buy a new dragon, note that the seller must authorize this contract to manage the token
+	/// @param _dragonId The id of the dragon to buy - dragon id references a sell order
+	function buyDragon(uint256 _dragonId) external payable {
+		require(dragonTokenAddress != address(0), 'Dragon address address cannot be empty');
 
-	/// @notice To publish a product as a seller
-	/// @param _title The title of the product
-	/// @param _description The description of the product
-	/// @param _price The price of the product in ETH
-	/// @param _image The image URL of the product
-	function publishProduct(
-		string memory _title,
-		string memory _description,
-		uint256 _price,
-		string memory _image
-	) public {
-		require(bytes(_title).length > 0, 'The title cannot be empty');
-		require(bytes(_description).length > 0, 'The description cannot be empty');
-		require(_price > 0, 'The price cannot be empty');
-		require(bytes(_image).length > 0, 'The image cannot be empty');
-		Product memory p = Product(lastId, _title, _description, now, msg.sender, _price, _image);
-		products.push(p);
-		productById[lastId] = p;
+		SellOrder memory sellOrder = sellOrderById[_dragonId];
+		require(sellOrder.owner != address(0), 'A sell order for the dragon must exist to be purchased');
 
-		// TODO marketplace should not mint, it should get get tokens from ERC721 contract, so maybe we should publish from there
-		EcommerceToken(token).mint(address(this), lastId); // Create a newtoken for this product which will be owned by this contract until soldl
-		lastId++;
-	}
+		Sale memory sale = Sale(_dragonId, sellOrder.owner, msg.sender, sellOrder.price, sellOrder.title);
 
-	/// @notice To buy a new product, note that the seller must authorize this contract to manage the token
-	/// @param _id The id of the product to buy
-	/// @param _nameSurname The name and surname of the buyer
-	/// @param _lineOneDirection The first line for the user address
-	/// @param _lineTwoDirection The second, optional user address line
-	/// @param _city Buyer's city
-	/// @param _stateRegion The state or region where the buyer lives
-	/// @param _postalCode The postal code of his location
-	/// @param _country Buyer's country
-	/// @param _phone The optional phone number for the shipping company
-	function buyProduct(
-		uint256 _id,
-		string memory _nameSurname,
-		string memory _lineOneDirection,
-		string memory _lineTwoDirection,
-		bytes32 _city,
-		bytes32 _stateRegion,
-		uint256 _postalCode,
-		bytes32 _country,
-		uint256 _phone
-	) public payable {
-		// The line 2 address and phone are optional, the rest are mandatory
-		require(bytes(_nameSurname).length > 0, 'The name and surname must be set');
-		require(bytes(_lineOneDirection).length > 0, 'The line one direction must be set');
-		require(_city.length > 0, 'The city must be set');
-		require(_stateRegion.length > 0, 'The state or region must be set');
-		require(_postalCode > 0, 'The postal code must be set');
-		require(_country > 0, 'The country must be set');
-		Product memory p = productById[_id];
-		require(bytes(p.title).length > 0, 'The product must exist to be purchased');
-		Order memory newOrder = Order(
-			_id,
-			msg.sender,
-			_nameSurname,
-			_lineOneDirection,
-			_lineTwoDirection,
-			_city,
-			_stateRegion,
-			_postalCode,
-			_country,
-			_phone,
-			'pending'
-		);
-		require(msg.value >= p.price, 'The payment must be larger or equal than the products price');
-		// Delete the product from the array of products
-		for (uint256 i = 0; i < products.length; i++) {
-			if (products[i].id == _id) {
-				Product memory lastElement = products[products.length - 1];
-				products[i] = lastElement;
-				products.length--;
+		require(msg.value >= sellOrder.price, 'The payment must be larger or equal than the sell order price');
+
+		// We can charge a fee here
+
+		// Delete the sellOrder from the array of sellOrders
+		for (uint256 i = 0; i < sellOrders.length; i++) {
+			if (sellOrders[i].id == _dragonId) {
+				SellOrder memory lastElement = sellOrders[sellOrders.length - 1];
+				sellOrders[i] = lastElement;
+				sellOrders.length--;
 			}
 		}
 		// Return the excess ETH sent by the buyer
-		if (msg.value > p.price) {
-			msg.sender.transfer(msg.value - p.price);
+		if (msg.value > sellOrder.price) {
+			msg.sender.transfer(msg.value - sellOrder.price);
 		}
-		pendingSellerOrders[p.owner].push(newOrder);
-		pendingBuyerOrders[msg.sender].push(newOrder);
-		orderById[_id] = newOrder;
-		EcommerceToken(token).transferFrom(address(this), msg.sender, _id); //Transfer the product token to the new owner
-		p.owner.transfer(p.price);
+
+		salesHistory.push(sale);
+
+		//Transfer the token to the new owner
+		IERC721(dragonTokenAddress).safeTransferFrom(address(this), msg.sender, _dragonId);
+
+		sellOrder.owner.transfer(sellOrder.price);
 	}
 }

30e22fc977a8a949890810be1e96115c1ae52428
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2021-02-20 16:10:45 -0300

    Accounts provider added in guides

--- web/containers/guides/index.tsx
+++ web/containers/guides/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { ReactElement } from 'react'
 
+import GuidesCommons from './guides'
 import GuidesDesktop from './guides.desktop'
 import GuidesMobile from './guides.mobile'
 
 import { ISSRPropsDeviceOnly } from '../../types/server-side-props-device-only'
 
 export default function Guides({ deviceType }: ISSRPropsDeviceOnly): ReactElement {
-  return deviceType === 'desktop' ? <GuidesDesktop /> : <GuidesMobile />
+  return deviceType === 'desktop' ? (
+    <GuidesCommons>
+      <GuidesDesktop />
+    </GuidesCommons>
+  ) : (
+    <GuidesCommons>
+      <GuidesMobile />
+    </GuidesCommons>
+  )
 }

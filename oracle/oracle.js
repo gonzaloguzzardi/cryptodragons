4d0d8fbcaa48c5c36826638fb7fb8aed47d6ef55
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2021-02-24 11:38:36 -0300

    Mock oracle sidechain account

--- oracle/oracle.js
+++ oracle/oracle.js
@@ -31,7 +31,18 @@ listenSideChainEvents();
 
 // API ROUTES
 app.get('/api/dragons', getDragonsInGateways);
-app.get('/api/getOrCreateSideAccount', getOrCreateSideAccount);
+
+// @TODO: Uncomment next line and delete mocked return value
+// app.get('/api/getOrCreateSideAccount', getOrCreateSideAccount);
+app.get('/api/getOrCreateSideAccount', (req, res) => {
+  const account = {
+    mainAccount: req.query.account,
+    sidePrivateKey: '0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709',
+    sideAccount: '0xb8CE9ab6943e0eCED004cDe8e3bBed6568B2Fa01',
+    isFirst: true,
+  };
+  res.status(200).send(account);
+});
 
 // SERVER LISTEN
 const server = app.listen(oracleApiPort, () => {

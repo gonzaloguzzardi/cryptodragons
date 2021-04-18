4a466bc84363d9498b29ef0949623e54de9a94c3
nicomoccagatta <nmoccagatta@medallia.com>
2021-04-18 16:31:54 -0300

    [WEB]: Little fix in mainchain client connection

--- web/services/blockchain-interaction/mainchain/client-factory.js
+++ web/services/blockchain-interaction/mainchain/client-factory.js
@@ -2,8 +2,7 @@
 import Web3 from 'web3';
 import detectEthereumProvider from '@metamask/detect-provider';
 
-async function getMainNetTokenContract(web3js) {
-  const networkId = await web3js.eth.net.getId();
+async function getMainNetTokenContract(web3js, networkId) {
   const MainchainDragonTokenJson = require('../../../contracts/MainnetTransferableDragon.json');
   return new web3js.eth.Contract(
     MainchainDragonTokenJson.abi,
@@ -11,8 +10,7 @@ async function getMainNetTokenContract(web3js) {
   );
 }
 
-async function getMainNetGatewayContract(web3js) {
-  const networkId = await web3js.eth.net.getId();
+async function getMainNetGatewayContract(web3js, networkId) {
   const GatewayJson = require('../../../contracts/MainnetGateway.json');
   return new web3js.eth.Contract(GatewayJson.abi, GatewayJson.networks[networkId].address);
 }
@@ -31,11 +29,17 @@ export default async function clientFactory() {
 
   const web3js = new Web3(provider);
 
+  const networkId = await web3js.eth.net.getId();
+  if (networkId !== 12345) {
+    alert("Connect to BFA mainnet with metamask");
+    return Promise.resolve({});
+  }
+
   return Promise.all([
     ethereum.request({ method: 'eth_accounts' }),
     ethereum.request({ method: 'eth_chainId' }),
-    getMainNetTokenContract(web3js),
-    getMainNetGatewayContract(web3js),
+    getMainNetTokenContract(web3js, networkId),
+    getMainNetGatewayContract(web3js, networkId),
   ])
     .then((values) => ({
       account: values[0][0],

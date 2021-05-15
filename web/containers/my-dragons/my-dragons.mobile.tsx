b47119e8cbee30ef5f65574bed200a92af77b1a4
nicomoccagatta <nmoccagatta@medallia.com>
2021-05-15 20:42:27 -0300

    Logo Toolbar Detail

--- web/containers/my-dragons/my-dragons.mobile.tsx
+++ web/containers/my-dragons/my-dragons.mobile.tsx
@@ -3,13 +3,16 @@ import React, { ReactElement } from 'react'
 import { withAccountsHOC } from '../../hooks/accounts-context'
 
 import AppToolbar from '../../components/app-toolbar'
+// import MyDragonsSearchContainerMobile from '../../components/my-dragons/search-container/mobile'
+// import MyDragonsGridViewMobile from '../../components/my-dragons/grid-view/desktop'
 import FooterMobile from '../../components/footer/mobile'
 
 function MyDragonsMobile({ accountsState }): ReactElement {
   return (
     <>
       <AppToolbar deviceType="mobile" section="my-dragons" accountsState={accountsState} />
-      <p style={{ height: '70vh' }}>My Dragons</p>
+      {/* <MyDragonsSearchContainerMobile />
+      <MyDragonsGridViewMobile /> */}
       <FooterMobile />
     </>
   )

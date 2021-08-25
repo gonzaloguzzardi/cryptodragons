09e21b657fb15bdbf463e4e5607f5ea9d657fca2
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2021-08-25 16:21:21 -0300

    Changed solidity imported version

--- truffle-project/contracts/public/DragonApi.sol
+++ truffle-project/contracts/public/DragonApi.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0 License
 
-pragma solidity 0.8.4;
+pragma solidity ^0.8.0;
 
 import '../common/token/IERC721Enumerable.sol';
 import '../common/libraries/DragonLibrary.sol';
--- truffle-project/contracts/public/MainnetDragonApi.sol
+++ truffle-project/contracts/public/MainnetDragonApi.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0 License
 
-pragma solidity 0.8.4;
+pragma solidity ^0.8.0;
 
 import './DragonApi.sol';
 import '../common/ownership/Ownable.sol';

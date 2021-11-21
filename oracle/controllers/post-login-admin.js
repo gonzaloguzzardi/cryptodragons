29e3e6b449d3c092114eabdd9ceab6f189567f26
Nico Moccagatta <nicomoccagatta@gmail.com>
2021-11-21 13:58:11 -0300

    Send correct jwt token from oracle

--- oracle/controllers/post-login-admin.js
+++ oracle/controllers/post-login-admin.js
@@ -1,18 +1,22 @@
-const {
-  ADMIN_USERNAME,
-  ADMIN_PASSWORD,
-} = require('../config');
+const jwt = require('jsonwebtoken');
+const { ADMIN_USERNAME, ADMIN_PASSWORD, ADMIN_SECRET } = require('../config');
 
 async function postLoginAdmin(req, res) {
   const { username, password } = req.body;
 
-  if ((username === ADMIN_USERNAME) && (password === ADMIN_PASSWORD)) {
-    res.status(200).send({ token: "TOKEN-MOCC" });
+  if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
+    const user = {
+      username,
+      role: 'admin',
+    };
+    const token = jwt.sign(user, ADMIN_SECRET, { expiresIn: 600 });
+
+    res.status(200).send({ token });
   }
 
   res.status(401).send();
 }
 
 module.exports = {
-    postLoginAdmin,
+  postLoginAdmin,
 };

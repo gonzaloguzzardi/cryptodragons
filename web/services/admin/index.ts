33eb90f54e3af10652343109ed442fb5d6b9a3dc
Nico Moccagatta <nicomoccagatta@gmail.com>
2021-11-23 18:48:42 -0300

    Disable cache for testing jwt session

--- web/services/admin/index.ts
+++ web/services/admin/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 type GetSessionDataResponse = any // @TODO: Type this
 export const getSessionAdmin = (token: string): Promise<GetSessionDataResponse> =>
   axios
-    .get('/api/admin/session/test', { headers: { Authorization: token } })
+    .get(`/api/admin/session/test?cache=${Math.random()}`, { headers: { Authorization: token } })
     .then(({ data }) => data)
     .catch(({ response }) => {
       throw new Error(response.data.error)

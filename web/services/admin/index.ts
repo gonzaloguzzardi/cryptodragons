b31877c65ad0126510d90efea8b295092f6d5c59
Nico Moccagatta <nicomoccagatta@gmail.com>
2021-11-23 18:02:33 -0300

    Finish login logic + redirect

--- web/services/admin/index.ts
+++ web/services/admin/index.ts
@@ -1,14 +1,22 @@
 import axios from 'axios'
 
-type Data = {
+type GetSessionDataResponse = any // @TODO: Type this
+export const getSessionAdmin = (token: string): Promise<GetSessionDataResponse> =>
+  axios
+    .get('/api/admin/session/test', { headers: { Authorization: token } })
+    .then(({ data }) => data)
+    .catch(({ response }) => {
+      throw new Error(response.data.error)
+    })
+
+type PostLoginDataResponse = {
   token?: string
   error?: string
 }
-
-export const postLoginData = (username: string, password: string): Promise<Data> =>
+export const postLoginData = (username: string, password: string): Promise<PostLoginDataResponse> =>
   axios
-    .post(`/api/admin/login`, { username, password })
-    .then(({ data }) => ({ token: data.token }))
-    .catch((err) => {
-      throw new Error(err.response.data.error)
+    .post('/api/admin/login', { username, password })
+    .then(({ data }) => data)
+    .catch(({ response }) => {
+      throw new Error(response.data.error)
     })

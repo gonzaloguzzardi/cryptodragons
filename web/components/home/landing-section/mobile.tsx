9b3c1755daf7350b638ec616bbf54cdf4a35dae8
Nico Moccagatta <nicomoccagatta@gmail.com>
2022-04-19 03:03:23 -0300

    [WEB] Footer redesign - fix home landing

--- web/components/home/landing-section/mobile.tsx
+++ web/components/home/landing-section/mobile.tsx
@@ -7,13 +7,14 @@ import styles from './mobile.module.scss'
 
 export default function LandingSection(): ReactElement {
   return (
-    <div className={styles.main}>
+    <div>
       <Parallax
         blur={1}
         bgImage="/assets/home/background_dragon-land.jpg"
         bgImageAlt="background landing"
         strength={120}
       >
+        <div className={styles.main}>
           <div className={styles.title}>
             <Typography variant="h4" component="h2">
               CryptoDragons
@@ -22,6 +23,7 @@ export default function LandingSection(): ReactElement {
           <div className={styles.images}>
             <img src="/assets/home/dragon-1.png" alt="" width="300px" height="300px" />
           </div>
+        </div>
       </Parallax>
     </div>
   )

e0a7ac3aeb67b01beedc8770b09b45516858e7cd
Nico Moccagatta <nicomoccagatta@gmail.com>
2022-04-21 17:38:18 -0300

    truffle-config ETIMEDOUT error fix(?

--- truffle-project/truffle-config.js
+++ truffle-project/truffle-config.js
@@ -91,8 +91,9 @@ module.exports = {
 			network_id: '12345',
 			gas: 5227597,
 			from: '0x28863498efede12296888f7ca6cf0b94974fbdbc',
-			networkCheckTimeout: 1000000,
-      timeoutBlocks: 200
+			skipDryRun: true,
+			networkCheckTimeout: 9000000,
+      timeoutBlocks: 90000
 		},
 	},
 	plugins: ['truffle-contract-size'],

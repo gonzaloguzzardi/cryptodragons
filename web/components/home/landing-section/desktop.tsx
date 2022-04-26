f6aa0c083540b4d7d2b1c997c80a687680ec6c7a
Nico Moccagatta <nicomoccagatta@gmail.com>
2022-04-26 17:49:24 -0300

    Remove parallax

--- web/components/home/landing-section/desktop.tsx
+++ web/components/home/landing-section/desktop.tsx
@@ -1,5 +1,3 @@
-// @TODO: Fix this
-// import { Parallax } from 'react-parallax'
 import Typography from '@mui/material/Typography'
 
 import { ReactElement } from 'react'
@@ -8,13 +6,7 @@ import styles from './desktop.module.scss'
 
 export default function LandingSection(): ReactElement {
   return (
-    <div>
-      {/* <Parallax
-        blur={1}
-        bgImage="/assets/home/background_dragon-land.jpg"
-        bgImageAlt="background landing"
-        strength={120}
-      > */}
+    <div className={styles.landingImage}>
       <div className={styles.main}>
         <div className={styles.title}>
           <Typography variant="h2">CryptoDragons</Typography>
@@ -23,7 +15,6 @@ export default function LandingSection(): ReactElement {
           <img src="/assets/home/dragon-1.png" alt="" width="400px" height="400px" />
         </div>
       </div>
-    {/* </Parallax> */}
     </div>
   )
 }
--- web/components/home/landing-section/mobile.tsx
+++ web/components/home/landing-section/mobile.tsx
@@ -1,31 +1,22 @@
 import { ReactElement } from 'react'
 
-// @TODO: Fix this
-// import { Parallax } from 'react-parallax'
 import Typography from '@mui/material/Typography'
 
 import styles from './mobile.module.scss'
 
 export default function LandingSection(): ReactElement {
   return (
-    <div>
-      {/* <Parallax
-        blur={1}
-        bgImage="/assets/home/background_dragon-land.jpg"
-        bgImageAlt="background landing"
-        strength={120}
-      > */}
-        <div className={styles.main}>
-          <div className={styles.title}>
-            <Typography variant="h4" component="h2">
-              CryptoDragons
-            </Typography>
-          </div>
-          <div className={styles.images}>
-            <img src="/assets/home/dragon-1.png" alt="" width="300px" height="300px" />
-          </div>
+    <div className={styles.landingImage}>
+      <div className={styles.main}>
+        <div className={styles.title}>
+          <Typography variant="h4" component="h2">
+            CryptoDragons
+          </Typography>
         </div>
-      {/* </Parallax> */}
+        <div className={styles.images}>
+          <img src="/assets/home/dragon-1.png" alt="" width="300px" height="300px" />
+        </div>
+      </div>
     </div>
   )
 }

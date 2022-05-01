2f16b10d9f1b19e7b6e1d3a59b5a2c295f05a564
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2022-04-30 23:58:29 -0300

    Added test

--- truffle-project/test/MainnetMarketplace.js
+++ truffle-project/test/MainnetMarketplace.js
@@ -1,18 +1,35 @@
+const {expectRevert} = require('@openzeppelin/test-helpers');
+
 const MainnetMarketplace = artifacts.require('MainnetMarketplace');
+const MainnetTransferableDragon = artifacts.require('MainnetTransferableDragon');
 
 contract('MainnetMarketplace', (accounts) => {
-    let marketplace;
     const DRAGON_TOKEN_ADDRESS = '0x1234567890123456789012345678901234567891';
 
+    const GATEWAY_ADDRESS = '0x1234567890123456789012345678901234567892';
+    const DRAGON_DECODER_ADDRESS = '0x1234567890123456789012345678901234567893';
+    const BLOCKCHAIN_ID = '1';
+
+    const PRICE = '100';
+
+    let ownerAccount    = accounts[0];
+    let nonOwnerAccount = accounts[1];
+
+    let marketplaceContract;
+    let tokenContract;
+
     beforeEach(async () => {
-        marketplace = await MainnetMarketplace.new(DRAGON_TOKEN_ADDRESS);
+        marketplaceContract = await MainnetMarketplace.new(DRAGON_TOKEN_ADDRESS);
+        tokenContract = await MainnetTransferableDragon.new(GATEWAY_ADDRESS, DRAGON_DECODER_ADDRESS, BLOCKCHAIN_ID);
+
+        await tokenContract.createDragon("test", 0, 0, 0, {
+            from: ownerAccount
+        });
     });
 
-    it('Should initialize dragonTokenAddress with provided value', async () => {
-        const tx = await marketplace.dragonTokenAddress({
-            from: accounts[0]
-        }).then((dragonTokenAddress) => {
-            assert.equal(dragonTokenAddress, DRAGON_TOKEN_ADDRESS);
-        });
+    it('listToken should revert if token was not approved by owner', async () => {
+        await tryCatch(marketplaceContract.listToken(DRAGON_TOKEN_ADDRESS, 0, PRICE, {
+            from: nonOwnerAccount
+        }), errTypes.revert);
     });
 });
